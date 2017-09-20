eb9e64427e9d69b8e048e30c0ae1ceb07a5a095b
napochi <31980392+napochi@users.noreply.github.com>
2017-09-20 17:00:55 +0800

    Exercise_02.py

---
+++ 动态打印MQY.py
@@ -0,0 +1,29 @@
+import time
+a1="#       #  #####   #     #"
+a2="##     ## #     #   #   #"
+a3="# #   # # #     #    # #"
+a4="#  # #  # #   ###     #"
+a5="#   #   #  ##### #    #"
+
+t=0
+if t<6:
+    if t<1:
+        print(a1)
+        t+=1
+        time.sleep(1.5)
+    if t<2:
+        print(a2)
+        t+=1
+        time.sleep(1.5)
+    if t<3:
+        print(a3)
+        t+=1
+        time.sleep(1.5)
+    if t<4:
+        print(a4)
+        t+=1
+        time.sleep(1.5)
+    if t<5:
+        print(a5)
+        t+=1
+        time.sleep(1.5)
